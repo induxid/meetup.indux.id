ea9ba0c239a814ff354f578447f06ed72b145811
Yuri Pratama <hello@yuripertamax.me>
2017-02-07 01:34:04 +0700

    Init

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var appJsonFile = 'app.json';
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+var appJsonData = fs.readFileSync(appJsonFile);
+var appData = JSON.parse(appJsonData);
+var meetupData = appData.meetup;
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.redirect(appData.default_url);
 });
+
+Object.keys(meetupData).forEach(function(key) {
+
+  var slug = meetupData[key].slug;
+  var url = meetupData[key].url;
+
+  app.get( '/' + slug, function(request, response) {
+    response.redirect(url);
+  });
+
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
